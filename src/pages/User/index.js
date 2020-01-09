ea4277527395665db582a9de5bf36d257547f75b
Ygor <Ygor>
2020-01-09 17:01:18 -0300

    listando repositorios

--- src/pages/User/index.js
+++ src/pages/User/index.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { View } from 'react-native';
 import api from '../../services/api';
-
-// import { Container } from './styles';
+import { 
+  Container,
+  Header, 
+  Avatar, 
+  Name, 
+  Bio,
+  Stars,
+  Starred,
+  OwnerAvatar,
+  Info,
+  Title,
+  Author,
+  } from '../Main/styles';
 
 export default class User extends Component {
   static navigationOptions = ({ navigation }) => ({
@@ -30,7 +41,33 @@ export default class User extends Component {
   }
   
   render() {
+    const { navigation } = this.props;
     const { stars } = this.state;
-  return <View />; 
+
+    const user = navigation.getParam('use')
+    
+    return(
+      <Container>
+        <Header>
+        <Avatar source={{ uri: user.avatar }} />
+           <Name>{user.name}</Name>
+           <Bio>{user.bio}</Bio>
+        </Header>
+
+        <Stars
+          data={stars}
+          keyExtractor={ star => String(star.id)}
+          renderItem={({ Item }) => (
+            <Starred>
+              <OwnerAvatar source={{ uri: Item.owner.avatar_url }} />
+              <Info>
+                <Title>{Item.name}</Title>
+                <Author>{Item.owner.login}</Author>
+              </Info>
+            </Starred>
+          )}
+        />
+      </Container>
+    )  
    }
 }
